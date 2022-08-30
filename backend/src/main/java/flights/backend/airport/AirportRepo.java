327fb521ac3696efd2c01431adaf104706a09497
irenepapaspyratos <101578008+irenepapaspyratos@users.noreply.github.com>
2022-08-30 12:19:26 +0200

    Update AirportRepo.java

--- backend/src/main/java/flights/backend/airport/AirportRepo.java
+++ backend/src/main/java/flights/backend/airport/AirportRepo.java
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@Document(collection = "airports")
+@Document("airports")
 public interface AirportRepo extends MongoRepository<Airport, String> {
     Airport findAirportByIata(String iata);
 
