3ec245badfd2fe25181daf9489d32491b37c887c
irenepapaspyratos <101578008+irenepapaspyratos@users.noreply.github.com>
2022-08-30 12:26:38 +0200

    Remove @Document from AirportRepo

--- backend/src/main/java/flights/backend/airport/AirportRepo.java
+++ backend/src/main/java/flights/backend/airport/AirportRepo.java
@@ -5,7 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@Document("airports")
 public interface AirportRepo extends MongoRepository<Airport, String> {
     Airport findAirportByIata(String iata);
 
