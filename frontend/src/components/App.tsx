8f6e73ed7588529c8052a8b4bfd9f502fe9ed056
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2022-09-06 20:27:18 +0200

    Implement Zustand for state management

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import GlobalStyle from "./ui/GlobalStyle";
 import Layout from "./Layout";
-import {useTranslation} from "react-i18next";
 
 export default function App() {
-    const translationHook = useTranslation();
 
     return (
         <>
             <GlobalStyle/>
-            <Layout translationHook={translationHook}/>
+            <Layout/>
         </>
     );
 }
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,15 +1,14 @@
 import Navigation from "./Navigation";
 import HeaderStyled from "./ui/Header.styled";
-import {UseTranslationResponse} from "react-i18next";
 import FlagsContainer from "./FlagsContainer";
 import React from "react";
 
-export default function Header(props: { translationHook: UseTranslationResponse<"translation"> }) {
+export default function Header() {
     return (
         <HeaderStyled>
-            <FlagsContainer translationHook={props.translationHook}/>
+            <FlagsContainer/>
             <h1>FLIGHTS</h1>
-            <Navigation translationHook={props.translationHook}/>
+            <Navigation/>
         </HeaderStyled>
     );
 }
