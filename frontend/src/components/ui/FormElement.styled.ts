7d9bf5b22e426364e207f970cde1a4cda274fab2
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2022-09-10 19:09:18 +0200

    Add searchBar styling

--- frontend/src/components/ui/FormElement.styled.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-const FormElementStyled = styled.div<{ variant: string }>`
-    border: black;
-    background-color: #61dafb;
-`;
-
-export default FormElementStyled;
