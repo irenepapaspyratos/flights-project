cf80eb71d15bc3a64b011155d897e7b22e36c71d
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2022-09-11 17:05:30 +0200

    Beautify Navigation

--- frontend/src/components/ui/Navigation.styled.ts
+++ frontend/src/components/ui/Navigation.styled.ts
@@ -3,11 +3,11 @@ import styled, {css} from "styled-components";
 const NavigationStyled = styled.nav<{ toggled: boolean }>`
     position: fixed;
     top: 1vmin;
-    left: -54vmin;
-    width: 50vmin;
-    height: 75vh;
-    background-color: #141B20AD;
-    backdrop-filter: blur(15px);
+    left: -58vmin;
+    width: max-content;
+    height: 45vh;
+    background-color: #001D295E;
+    backdrop-filter: blur(20px);
     border-radius: 5px;
     border: 1px solid #646464;
     box-shadow: 10px 10px 15px;
@@ -15,11 +15,11 @@ const NavigationStyled = styled.nav<{ toggled: boolean }>`
     flex-direction: column;
     justify-content: flex-start;
     align-items: flex-end;
-    padding-top: 2rem;
+    padding: 3rem 1rem 2rem 2rem;
     gap: 1rem;
     font-size: larger;
     transform: ${({toggled}) =>
-            toggled && css`translateX(55vmin);`};
+            toggled && css`translateX(59vmin);`};
     transition: transform 0.300s ease-in-out;
     z-index: 990;
 
