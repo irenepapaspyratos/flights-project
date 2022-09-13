3a5963812f0e4b71799002cc1f3d2712791b01fd
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2022-09-14 01:46:21 +0200

    Fix: CodeSmells

--- backend/src/main/java/flights/backend/airport/AirportRepo.java
+++ backend/src/main/java/flights/backend/airport/AirportRepo.java
@@ -1,6 +1,5 @@
 package flights.backend.airport;
 
-import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
