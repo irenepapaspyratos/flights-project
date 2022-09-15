12c0c39481b2a0fd1ad960415700ba6a716917a1
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2022-09-16 01:36:53 +0200

    Finish styling Home & FlightList

--- frontend/src/components/ui/Button.styled.ts
+++ frontend/src/components/ui/Button.styled.ts
@@ -1,8 +1,9 @@
 import styled, {css} from 'styled-components';
 
-const Button = styled.button<{ variant: string }>`
+const Button = styled.button<{ variant: string, onClick?: () => void }>`
     border-radius: 2.5px;
     background: none;
+    text-align: center;
 
     :hover {
         box-shadow: 2px 2px 5px #282c34;
@@ -27,10 +28,25 @@ const Button = styled.button<{ variant: string }>`
     ${({variant}) =>
             variant === 'search' &&
             css`
-                background-color: #cb8a04;
-                border-color: #513700ad;
+                background-image: linear-gradient(#e89d02 20%, #352401);
+                border-color: #e3bc6c #352401 #352401 #b5841f;
                 height: 1.9rem;
                 width: auto;
+                min-width: 21vmin;
+                padding: 0 2%;
+                margin: auto 2vmin auto auto;
+                font-weight: bold;
+                color: var(--color-primary-light);
+                text-shadow: 1px 1px 2px var(--color-primary-dark);
+            `};
+
+    ${({variant}) =>
+            variant === 'flight' &&
+            css`
+                background-image: linear-gradient(#e89d02 20%, #352401);
+                border-color: #e3bc6c #352401 #352401 #b5841f;
+                height: 2.5rem;
+                width: min-content;
                 min-width: 21vmin;
                 padding: 0 2%;
                 margin: auto 2vmin auto auto;
--- frontend/src/components/ui/Header.styled.ts
+++ frontend/src/components/ui/Header.styled.ts
@@ -1,34 +1,27 @@
-import styled, {css} from "styled-components";
+import styled from "styled-components";
 
 const HeaderStyled = styled.header<{ path: string }>`
     text-align: center;
     width: 100%;
-    padding: 0.5vmin 0.5vmin 2vmin 0.5vmin;
+    padding: 0.5vmin 0.5vmin 5vmin 0.5vmin;
     position: sticky;
     top: 0;
     display: flex;
     flex-direction: column;
     justify-content: center;
     color: var(--color-primary-light);
-    background-attachment: fixed;
     z-index: 900;
 
-    ${({path}) =>
-            path === '/' && css`
-                background-image: url("/images/BirdPlaneSearching.jpg");
-                background-size: cover;
-                background-position: center;`};
+    box-shadow: 6px 15px 20px var(--color-primary-dark);
+    border-radius: 3px;
+    background-image: linear-gradient(#3278a4 -10%, #090b1c);
 
-    ${({path}) =>
-            path === '/flightlist' && css`
-                background-image: url("/images/BirdBeachRelaxing.jpg");
-                background-size: auto 300%;
-                background-position: 50% 70%;
-            `};
-
-
-    h1 {
+    h1, h2 {
         text-shadow: var(--shadow-text);
+    }
+
+    h2 {
+        font-size: 1rem;
     }
 `;
 
